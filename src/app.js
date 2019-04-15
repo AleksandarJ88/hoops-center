6c2dc0442a0cea58631081835cd0310223e85f87
unknown <jamina.aleksandar@gmail.com>
2019-04-16 00:18:20 +0200

    folder marked as git repository. app initiated.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+
+ReactDOM.render(<p>App Initiated!</p>, document.getElementById("app"));
