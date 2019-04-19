a6735f593db49cbdd7f97e2f08c6a1a39d988e32
unknown <jamina.aleksandar@gmail.com>
2019-04-20 00:03:25 +0200

    all pages displayed with dummy text. nav links added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => (
-  <h1>Hoops Center</h1>
+  <header>
+    <h1>Hoops Center</h1>
+    <NavLink to="/" exact="true">Home</NavLink>
+    <NavLink to="/search">Search</NavLink>
+    <NavLink to="/compare">Compare</NavLink>
+  </header>
 );
 
 export default Header;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,20 @@ import React from "react";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "../components/HomePage";
+import SearchPage from "../components/SearchPage";
+import ComparePage from "../components/ComparePage";
+import NotFoundPage from "../components/NotFoundPage";
+import Header from "../components/Header";
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
+      <Header/>
       <Switch>
         <Route path="/" component={HomePage} exact={true} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/compare" component={ComparePage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </div>
   </BrowserRouter>
