bc884ae19be8e3a9d3f36756fa0bd91ce0a91390
unknown <jamina.aleksandar@gmail.com>
2019-05-25 15:35:47 +0200

    started styling the app. header and nav links styling completed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => (
-  <header>
+  <header className="header">
     <h1>Hoops Center</h1>
-    <NavLink to="/" exact={true}>Home</NavLink>
-    <NavLink to="/search">Player Search</NavLink>
-    <NavLink to="/compare">Compare Page</NavLink>
+    <NavLink className="header__navLink" activeClassName="header__activeLink" to="/" exact={true}>Home</NavLink>
+    <NavLink className="header__navLink" activeClassName="header__activeLink" to="/search">Player Search</NavLink>
+    <NavLink className="header__navLink" activeClassName="header__activeLink" to="/compare">Compare Page</NavLink>
   </header>
 );
 
