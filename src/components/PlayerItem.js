2f0054698e2ecb0b108ea2c18cff0ed8cb771c1f
unknown <jamina.aleksandar@gmail.com>
2019-05-28 01:39:44 +0200

    completed player card styles.

--- src/components/PlayerItem.js
+++ src/components/PlayerItem.js
@@ -5,13 +5,15 @@ const PlayerItem = (props) => (
   <div>
     {props.player.id &&
       <div className="player">
-        <p>{props.player.firstName} {props.player.lastName}</p>
-        <p>{props.player.team}</p>
-        <p>Season 2018/2019</p>
+        <div className="player__info">
+          <p>{props.player.firstName} {props.player.lastName}</p>
+          <p>{props.player.team}</p>
+          <p>Season 2018/2019</p>
+        </div>
         <p>Games played: {props.player.gamesPlayed}</p>
-        <p>points per game: {props.player.avgPts}</p>
-        <p>assists per game: {props.player.avgAst}</p>
-        <p>rebounds per game: {props.player.avgReb}</p>
+        <p className="player__stats">points per game: {props.player.avgPts}</p>
+        <p className="player__stats">assists per game: {props.player.avgAst}</p>
+        <p className="player__stats">rebounds per game: {props.player.avgReb}</p>
       </div>}
   </div>
 );
