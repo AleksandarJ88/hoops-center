26d305420e891c6f6ac16b0efaeb143e60829ae3
unknown <jamina.aleksandar@gmail.com>
2019-05-30 01:12:09 +0200

    app made responsive and mobile friendly using flexbox.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import { NavLink } from "react-router-dom";
 const Header = () => (
   <header className="header">
     <h1><i className="fas fa-basketball-ball"></i> Hoops Center</h1>
-    <NavLink className="header__nav-link" activeClassName="header__active-link" to="/" exact={true}><i className="fas fa-home"></i> Home</NavLink>
-    <NavLink className="header__nav-link" activeClassName="header__active-link" to="/search"><i className="fas fa-search"></i> Player Search</NavLink>
-    <NavLink className="header__nav-link" activeClassName="header__active-link" to="/compare"><i className="fas fa-stream"></i> Compare Page</NavLink>
+    <div className="header__nav-bar">
+      <NavLink className="header__nav-link" activeClassName="header__active-link" to="/" exact={true}><i className="fas fa-home"></i> Home</NavLink>
+      <NavLink className="header__nav-link" activeClassName="header__active-link" to="/search"><i className="fas fa-search"></i> Player Search</NavLink>
+      <NavLink className="header__nav-link" activeClassName="header__active-link" to="/compare"><i className="fas fa-stream"></i> Compare Page</NavLink>
+    </div>
   </header>
 );
 
